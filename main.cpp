e9235d8e65a27c3204e55eeca07d8c848c67da76
Maral Mordad <maralmordad78@gmail.com>
2020-03-22 23:18:25 +0430

    define constructor

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 #include "person.h"
 
-int main() {
-    person maral;
-    maral.firstName="Maral";
-    maral.lastName="Mordad";
-    maral.age=20;
-    maral.socialNumber="1111";
 
-    std::cout <<"firstName:  "<<maral.firstName << std::endl;
-    std::cout <<"\nlastName:  "<<maral.lastName << std::endl;
-    std::cout <<"\n age:  "<<maral.age << std::endl;
-    std::cout <<"\nsocialNumber::  "<<maral.socialNumber << std::endl;
+int main() {
+person maral("maral","mordad",12,"1111");
+    std::cout <<"\nage::  "<<maral.set_age() << std::endl;
+    std::cout <<"\nname::  "<<maral.set_name() << std::endl;
     return 0;
 }
--- person.h
+++ person.h
@@ -4,11 +4,62 @@
 // Created by maral on 3/18/2020.
 //
 class person{
-public:
+private:
     std::string firstName;
     std::string lastName;
     int age;
     std::string socialNumber;
+
+public:
+    person()
+    {
+        firstName = "not assigned";
+        lastName = "not assigned";
+        socialNumber = "not assigned";
+        age = -1;
+
+    }
+
+    person(std::string a,std::string b,int c ,std::string d)
+    {
+        firstName = a;
+        lastName = b;
+        age = c;
+        socialNumber = d;
+
+    }
+
+    person(const person & object)
+    {
+        firstName = object.firstName;
+        lastName = object.lastName;
+        age = object.age;
+        socialNumber = object.socialNumber;
+    }
+
+
+
+
+
+    std::string set_name()
+    {
+        return lastName;
+
+
+    }
+    int set_age()
+    {
+        return age;
+    }
+/*
+    person(std::string e,std::string f,int g,std::string h)
+    {
+        firstName = e;
+        lastName = f;
+        age = g;
+        socialNumber = h;
+    }
+    */
 };
 #ifndef PART1_PERSON_H
 #define PART1_PERSON_H
